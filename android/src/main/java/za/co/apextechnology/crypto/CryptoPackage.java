d8127be1b547f215c34202cd2aa374cfe2ab8407
Zayin Krige <zkrige@users.noreply.github.com>
2018-06-11 12:01:06 +0200

    Update CryptoPackage.java
    Remove deprecated override

--- android/src/main/java/za/co/apextechnology/crypto/CryptoPackage.java
+++ android/src/main/java/za/co/apextechnology/crypto/CryptoPackage.java
@@ -22,11 +22,6 @@ import java.util.List;
 public class CryptoPackage implements ReactPackage {
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
